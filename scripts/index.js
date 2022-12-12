338826b17d99b078c856c142e8925b963cf79d58
Konstantin Radchenko <kostya.rad3@yandex.ru>
2022-12-12 15:49:18 +0300

    fixed all remarks

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,42 @@
-// find edit button and popup
-let editButton = document.querySelector('.button_edit');
-let popup = document.querySelector('.popup');
-// open popup by click
-editButton.addEventListener('click', function() {
+// all elements
+const buttonEdit = document.querySelector('.button_type_edit');
+const popup = document.querySelector('.popup');
+const buttonClose = document.querySelector('.button_type_close');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__info');
+const nameInput = document.querySelector('#profile-name');
+const jobInput = document.querySelector('#profile-job');
+const form = document.querySelector('.form');
+
+// this function opens popup
+function openPopup() {
   popup.classList.add('popup_opened');
-});
-// find all other elements
-let closeButton = document.querySelector('.button_close');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__info');
-let nameInput = document.querySelector('#profile-name');
-let jobInput = document.querySelector('#profile-job');
-let saveButton = document.querySelector('.button_save');
-let form = document.querySelector('.form');
-// make form inputs filled with name and job
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-// this function closes popup by removing class
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+};
+
+// this function closes popup
 function closePopup() {
   popup.classList.remove('popup_opened');
 };
-// close popup by click on close button
-closeButton.addEventListener('click', function () {
-  event.preventDefault();
-  closePopup();
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-});
+
+// open popup by clicking on edit button
+buttonEdit.addEventListener('click', openPopup);
+
+// close popup by clicking on close button
+buttonClose.addEventListener('click', closePopup)
+
 // this function submits form
-function formSubmitHandler (evt) {
+function submitForm (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
+
 // submit form
-form.addEventListener('submit', formSubmitHandler);
-// add submitting by pressing Enter key
-form.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    form.addEventListener('submit', formSubmitHandler);
-  }
-});
+form.addEventListener('submit', submitForm);
+
 
 
 
