b285e7a35ae12aa20f3ce5867b298ab639f1133f
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-02-04 17:31:46 +0300

    fix: fixed overlay closing function and other minor errors

--- scripts/validation.js
+++ scripts/validation.js
@@ -12,8 +12,8 @@ function handleButtonState (form, obj) {
   const buttonSubmit = form.querySelector(obj.submitButtonSelector);
   const isFormValid = form.checkValidity();
   buttonSubmit.disabled = !isFormValid;
-  buttonSubmit.classList.toggle(obj.inactiveButtonClass, !isFormValid)
-}
+  buttonSubmit.classList.toggle(obj.inactiveButtonClass, !isFormValid);
+};
 // function shows error on input and span
 function showError (input, span, obj) {
   input.classList.add(obj.inputErrorClass);
@@ -31,27 +31,27 @@ function handleInputValidity (input, obj) {
   const inputNameAttribute = input.getAttribute('name');
   const errorSpan = document.getElementById(`${inputNameAttribute}-error`);
   if (input.validity.valid) {
-    hideError(input, errorSpan, obj)
+    hideError(input, errorSpan, obj);
   } else {
-    showError(input, errorSpan, obj)
-  }
-}
+    showError(input, errorSpan, obj);
+  };
+};
 // function sets listeners on every input
 function setInputsListeners (form, obj) {
   inputs.forEach(input => {
     input.addEventListener('input' , () => {
-      handleButtonState (form, obj)
-      handleInputValidity (input, obj)
-    })
-  })
-}
+      handleButtonState (form, obj);
+      handleInputValidity (input, obj);
+    });
+  });
+};
 // main function that checks all form validity
 function enableValidation(obj) {
   const forms = Array.from(document.querySelectorAll(obj.formSelector));
   forms.forEach(form => {
-    handleButtonState (form, obj)
-    setInputsListeners(form, obj)
-  })
+    handleButtonState (form, obj);
+    setInputsListeners(form, obj);
+  });
 };
 
 enableValidation(elementsForValidation);
